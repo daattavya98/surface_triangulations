4f73f1350aca18817a8466b88fe9b58305523b80
daattavya98 <da579@cam.ac.uk>
2024-11-14 11:45:11 +0000

    Fixing marllib issues

---
+++ ai_lakatos/formaliser_demo.py
@@ -0,0 +1,5 @@
+from lean_dojo import LeanGitRepo, trace
+
+repo = LeanGitRepo("https://github.com/leanprover-community/mathlib4", "d0257020513851aa276246e01c104b06ea39cf02")
+traced_repo = trace(repo)
+print(traced_repo.traced_files_graph)
