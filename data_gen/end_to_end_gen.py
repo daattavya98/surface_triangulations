0d0d6b812970bfa41bb4801d670a71ecff76fcde
daattavya98 <da579@cam.ac.uk>
2025-03-05 18:19:10 +0000

    SPHERE WORKING!!!! DON"T MESS WITH THIS

--- data_gen/end_to_end_gen.py
+++ data_gen/end_to_end_gen.py
@@ -1,6 +1,8 @@
 import os
-from typing import Tuple
+from itertools import combinations
+from typing import List, Tuple
 
+import matplotlib.pyplot as plt
 import numpy as np
 import toponetx as tnx
 from numpy.linalg import matrix_rank
@@ -180,6 +182,10 @@ def sample_random_vertices(
     n_second_square_interior: None | int = 0,
     n_cycle_3: None | int = 0,
     n_cycle_4: None | int = 0,
+    n_diagonal_1_square_1: None | int = 0,
+    n_diagonal_2_square_1: None | int = 0,
+    n_diagonal_1_square_2: None | int = 0,
+    n_diagonal_2_square_2: None | int = 0,
 ) -> np.ndarray:
     """
     This function samples random vertices from a uniform distribution over a square
@@ -276,6 +282,78 @@ def sample_random_vertices(
         bottom_edge_square_1_vertices = np.sort(bottom_edge_square_1_vertices, axis=0)
         bottom_edge_square_2_vertices = bottom_edge_square_1_vertices
 
+        # Create points along the diagonal_1 of square 1
+        diagonal_1_square_1 = np.zeros((n_diagonal_1_square_1, 2))
+        corner_diag_point1 = np.random.uniform(0, 0.05)
+        corner_diag_point2 = np.random.uniform(0.95, 1)
+        corner_diagonal_point1 = np.array([corner_diag_point1, corner_diag_point1])
+        corner_diagonal_point1 = corner_diagonal_point1.reshape(1, -1)
+        corner_diagonal_point2 = np.array([corner_diag_point2, corner_diag_point2])
+        corner_diagonal_point2 = corner_diagonal_point2.reshape(1, -1)
+        for i in range(n_diagonal_1_square_1):
+            diag_point = np.random.uniform(0, 1)
+            diagonal_1_square_1[i] = np.array([diag_point, diag_point])
+        diagonal_1_square_1 = np.concatenate(
+            [corner_diagonal_point1, diagonal_1_square_1]
+        )
+        diagonal_1_square_1 = np.concatenate(
+            [diagonal_1_square_1, corner_diagonal_point2]
+        )
+
+        # Create points along the diagonal_2 of square 1
+        diagonal_2_square_1 = np.zeros((n_diagonal_2_square_1, 2))
+        corner_diag_point1 = np.random.uniform(0, 0.05)
+        corner_diag_point2 = np.random.uniform(0.95, 1)
+        corner_diagonal_point1 = np.array([corner_diag_point1, 1 - corner_diag_point1])
+        corner_diagonal_point1 = corner_diagonal_point1.reshape(1, -1)
+        corner_diagonal_point2 = np.array([corner_diag_point2, 1 - corner_diag_point2])
+        corner_diagonal_point2 = corner_diagonal_point2.reshape(1, -1)
+        for i in range(n_diagonal_2_square_1):
+            diag_point = np.random.uniform(0, 1)
+            diagonal_2_square_1[i] = np.array([diag_point, 1 - diag_point])
+        diagonal_2_square_1 = np.concatenate(
+            [corner_diagonal_point1, diagonal_2_square_1]
+        )
+        diagonal_2_square_1 = np.concatenate(
+            [diagonal_2_square_1, corner_diagonal_point2]
+        )
+
+        # Create points along the diagonal_1 of square 2
+        diagonal_1_square_2 = np.zeros((n_diagonal_1_square_2, 2))
+        corner_diag_point1 = np.random.uniform(0, 0.05)
+        corner_diag_point2 = np.random.uniform(0.95, 1)
+        corner_diagonal_point1 = np.array([corner_diag_point1, corner_diag_point1])
+        corner_diagonal_point1 = corner_diagonal_point1.reshape(1, -1)
+        corner_diagonal_point2 = np.array([corner_diag_point2, corner_diag_point2])
+        corner_diagonal_point2 = corner_diagonal_point2.reshape(1, -1)
+        for i in range(n_diagonal_1_square_2):
+            diag_point = np.random.uniform(0, 1)
+            diagonal_1_square_2[i] = np.array([diag_point, diag_point])
+        diagonal_1_square_2 = np.concatenate(
+            [corner_diagonal_point1, diagonal_1_square_2]
+        )
+        diagonal_1_square_2 = np.concatenate(
+            [diagonal_1_square_2, corner_diagonal_point2]
+        )
+
+        # Create points along the diagonal_2 of square 2
+        diagonal_2_square_2 = np.zeros((n_diagonal_2_square_2, 2))
+        corner_diag_point1 = np.random.uniform(0, 0.05)
+        corner_diag_point2 = np.random.uniform(0.95, 1)
+        corner_diagonal_point1 = np.array([corner_diag_point1, 1 - corner_diag_point1])
+        corner_diagonal_point1 = corner_diagonal_point1.reshape(1, -1)
+        corner_diagonal_point2 = np.array([corner_diag_point2, 1 - corner_diag_point2])
+        corner_diagonal_point2 = corner_diagonal_point2.reshape(1, -1)
+        for i in range(n_diagonal_2_square_2):
+            diag_point = np.random.uniform(0, 1)
+            diagonal_2_square_2[i] = np.array([diag_point, 1 - diag_point])
+        diagonal_2_square_2 = np.concatenate(
+            [corner_diagonal_point1, diagonal_2_square_2]
+        )
+        diagonal_2_square_2 = np.concatenate(
+            [diagonal_2_square_2, corner_diagonal_point2]
+        )
+
         return [
             np.concatenate(
                 [
@@ -285,6 +363,8 @@ def sample_random_vertices(
                     top_edge_square_1_vertices,
                     bottom_edge_square_1_vertices,
                     n_interior,
+                    diagonal_1_square_1,
+                    diagonal_2_square_1,
                 ]
             ),
             np.concatenate(
@@ -295,6 +375,8 @@ def sample_random_vertices(
                     top_edge_square_2_vertices,
                     bottom_edge_square_2_vertices,
                     n_second_square_interior,
+                    diagonal_1_square_2,
+                    diagonal_2_square_2,
                 ]
             ),
         ]
@@ -380,6 +462,35 @@ def generate_genus_1_triangulations(
     return points, tri, adj_matrix, quotiented_adj_matrix
 
 
+# def fix_boundary_edges_genus_0(previous_tri_list: List, square_simplices: np.ndarray, new_label_counter: int):
+#     """
+#     Fix the gluing of the triangulation for the boundary edges in genus 0.
+
+#     Parameters
+#     ----------
+#     square_simplices : np.ndarray
+#         Second square simplices
+#     starting_new_label_counter : int
+#         Starting label for new vertices
+#     """
+#     triangles_to_remove_from_square_simplices = []
+#     previous_label_counter = new_label_counter
+#     previous_triangle = []
+#     for corner_el in range(4):
+#         for triangle in square_simplices:
+#             if corner_el in triangle:
+#                 remaining_indices = [other_el for other_el in triangle if other_el != corner_el]
+#                 new_triangle1 = [corner_el, remaining_indices[0], new_label_counter]
+#                 new_triangle2 = [corner_el, remaining_indices[1], new_label_counter]
+#                 previous_tri_list.append(new_triangle1)
+#                 previous_tri_list.append(new_triangle2)
+#                 previous_label_counter = new_label_counter
+#                 previous_triangle = triangle
+#                 new_label_counter += 1
+#                 triangles_to_remove_from_square_simplices.append(triangle)
+#             else:
+
+
 def construct_simplicial_complex(
     quotiented_adj_matrix: np.ndarray,
     simplices: np.ndarray,
@@ -444,6 +555,10 @@ def construct_simplicial_complex_genus_0(
     n_cycle_4: int,
     n_interior: int,
     n_interior_square_2: int,
+    n_diagonal_1_square_1: int,
+    n_diagonal_2_square_1: int,
+    n_diagonal_1_square_2: int,
+    n_diagonal_2_square_2: int,
 ) -> tnx.SimplicialComplex:
 
     points = sample_random_vertices(
@@ -454,6 +569,10 @@ def construct_simplicial_complex_genus_0(
         n_cycle_3=n_cycle_3,
         n_cycle_4=n_cycle_4,
         genus=0,
+        n_diagonal_1_square_1=n_diagonal_1_square_1,
+        n_diagonal_2_square_1=n_diagonal_2_square_1,
+        n_diagonal_1_square_2=n_diagonal_1_square_2,
+        n_diagonal_2_square_2=n_diagonal_2_square_2,
     )
 
     points_square_1 = points[0]
@@ -465,47 +584,111 @@ def construct_simplicial_complex_genus_0(
     two_simplex = []
     tri_list = tri_square_1.simplices.tolist()
 
-    second_square_interior_start_index = (
-        4 + n_cycle_1 + n_cycle_2 + n_cycle_3 + n_cycle_4 + n_interior
-    )
+    # second_square_interior_start_index = (
+    #     4 + n_cycle_1 + n_cycle_2 + n_cycle_3 + n_cycle_4 + n_interior
+    # )
 
+    extra_index_counter = (
+        4 + n_cycle_1 + n_cycle_2 + n_cycle_3 + n_cycle_4 + n_interior + 4
+    )
+    +n_diagonal_1_square_1 + n_diagonal_2_square_1
+    # # Check the double corner-boundary triangles and update the labelling to deal with this
+    # for tri1 in tri_square_1.simplices:
+    #     if any(el in [0, 1 , 2, 3] for el in tri1):
+    #         remaining_indices = [el for el in tri1 if el not in [0, 1, 2, 3]]
+    #         if not any(
+    #             el in range(4 + n_cycle_1 + n_cycle_2 + n_cycle_3 + n_cycle_4,
+    #                         4 + n_cycle_1 + n_cycle_2 + n_cycle_3 + n_cycle_4 + n_interior) for el in
+    #                   remaining_indices):
+
+    #             for triangle_2 in tri_square_2.simplices:
+    #                 if np.array_equal(tri1, triangle_2):
+    #                     corner_index = np.min(triangle_2)
+    #                     remaining_indices = [el for el in triangle_2 if el != corner_index]
+    #                     new_triangle_1 = [corner_index, remaining_indices[0], extra_index_counter]
+    #                     new_triangle_2 = [corner_index, remaining_indices[1], extra_index_counter]
+    #                     extra_index_counter += 1
+    #                     tri_list.append(new_triangle_1)
+    #                     tri_list.append(new_triangle_2)
+    #                     # Use boolean indexing to filter out the target array
+    #                     tri_square_2.simplices = np.array([row for row in tri_square_2.simplices
+    #                                                        if not np.array_equal(row, triangle_2)])
+
+    # for tri1 in tri_square_1.simplices:
+    #     for el in tri1:
+    #         if el + 1 in tri1:
+    #             # Deal with boundary-boundary edge case
+    #             # Check if such a triangle exists in the second square
+    #             for tri2 in tri_square_2.simplices:
+    #                 # if tri2 == tri1:
+    #                 pass
+
+    # else:
+    #     boundary_range = range(4, 4 + n_cycle_1 + n_cycle_2 + n_cycle_3 + n_cycle_4)
+    #     pairs = combinations(tri1, 2)
+
+    #     for pair in pairs:
+    #         flag = False
+    #         if all(el in boundary_range for el in pair):
+    #             boundary_pair_indices = pair
+    #             remaining_third_vertex = [el for el in tri1 if el not in boundary_range][0]
+    #             new_triangle_bnd_pair_1 = [boundary_pair_indices[0], boundary_pair_indices[1], extra_index_counter]
+    #             new_triangle_bnd_pair_2 = [boundary_pair_indices[0], remaining_third_vertex,  extra_index_counter]
+    #             new_triangle_bnd_pair_3 = [boundary_pair_indices[1], remaining_third_vertex,  extra_index_counter]
+    #             extra_index_counter += 1
+    #             tri_list.append(new_triangle_bnd_pair_1)
+    #             tri_list.append(new_triangle_bnd_pair_2)
+    #             tri_list.append(new_triangle_bnd_pair_3)
+    #             tri_list = [row for row in tri_list if not np.array_equal(row, tri1)]
+    #             flag = True
+    #             break
+    #         if flag:
+    #             break
+    list_of_index_replacements = []
     for triangle in tri_square_2.simplices:
         for el in triangle:
-            if el not in range(
+            if el in range(
                 4 + n_cycle_1 + n_cycle_2 + n_cycle_3 + n_cycle_4,
-                4 + n_cycle_1 + n_cycle_2 + n_cycle_3 + n_cycle_4 + n_interior_square_2,
+                8
+                + n_cycle_1
+                + n_cycle_2
+                + n_cycle_3
+                + n_cycle_4
+                + n_interior_square_2
+                + n_diagonal_1_square_2
+                + n_diagonal_2_square_2,
             ):
-                if not any(np.array_equal(arr, triangle) for arr in tri_list):
-                    tri_list.append(triangle)
-            else:
-                for i in range(
-                    4 + n_cycle_1 + n_cycle_2 + n_cycle_3 + n_cycle_4,
-                    4
-                    + n_cycle_1
-                    + n_cycle_2
-                    + n_cycle_3
-                    + n_cycle_4
-                    + n_interior_square_2,
-                ):
-                    if el == i:
-                        triangle[triangle == el] = (
-                            second_square_interior_start_index
-                            + i
-                            - n_cycle_1
-                            - n_cycle_2
-                            - n_cycle_3
-                            - n_cycle_4
-                            - 4
-                        )
-                if not any(np.array_equal(arr, triangle) for arr in tri_list):
-                    tri_list.append(triangle)
+                if len(list_of_index_replacements) == 0:
+                    triangle[triangle == el] = extra_index_counter + el - 1
+                    list_of_index_replacements.append(
+                        (el, extra_index_counter + el - 1)
+                    )
+                else:
+                    flag = False
+                    for row in list_of_index_replacements:
+                        if el == row[0]:
+                            triangle[triangle == el] = row[1]
+                            flag = True
+                            break
+                    if not flag:
+                        max_value = max(list_of_index_replacements, key=lambda x: x[1])[
+                            1
+                        ]
+                        triangle[triangle == el] = max_value + 1
+                        list_of_index_replacements.append((el, max_value + 1))
+
+        # if not any(np.array_equal(arr, triangle) for arr in tri_list):
+        tri_list.append(triangle)
 
     tri_list = list(set(tuple(triangle) for triangle in tri_list))
     two_simplex = np.array(tri_list)
 
     sc = tnx.SimplicialComplex(two_simplex)
 
-    return sc, tri_square_1, tri_square_2
+    tri_square_1 = Delaunay(points_square_1)
+    tri_square_2 = Delaunay(points_square_2)
+
+    return sc, tri_square_1, tri_square_2, points_square_1, points_square_2
 
 
 def create_link_graph(incident_edges, E, link_faces):
@@ -581,6 +764,8 @@ def check_surface_homeomorphic(vertex_edge_matrix, edge_face_matrix):
 
         # Create the adjacency matrix for the link graph
         link_graph = create_link_graph(incident_edges, E, link_faces)
+
+        # print(f"Link graph for vertex {vertex}:\n{link_graph}")
 
         # Check if the link graph is a single connected cycle
         n_components, _ = connected_components(
@@ -674,27 +859,40 @@ def generate_genus_0_datapoints(
     n_cycle_4: int = 4,
     n_interior: int = 3,
     n_interior_square_2: int = 4,
+    n_diagonal_1_square_1: int = 2,
+    n_diagonal_2_square_1: int = 3,
+    n_diagonal_1_square_2: int = 2,
+    n_diagonal_2_square_2: int = 3,
 ) -> Tuple[np.ndarray, np.ndarray]:
 
-    [sc_sphere, tri_square_1, tri_square_2] = construct_simplicial_complex_genus_0(
-        n_cycle_1=n_cycle_1,
-        n_cycle_2=n_cycle_2,
-        n_cycle_3=n_cycle_3,
-        n_cycle_4=n_cycle_4,
-        n_interior=n_interior,
-        n_interior_square_2=n_interior_square_2,
+    [sc_sphere, tri_square_1, tri_square_2, points_square_1, points_square_2] = (
+        construct_simplicial_complex_genus_0(
+            n_cycle_1=n_cycle_1,
+            n_cycle_2=n_cycle_2,
+            n_cycle_3=n_cycle_3,
+            n_cycle_4=n_cycle_4,
+            n_interior=n_interior,
+            n_interior_square_2=n_interior_square_2,
+            n_diagonal_1_square_1=n_diagonal_1_square_1,
+            n_diagonal_2_square_1=n_diagonal_2_square_1,
+            n_diagonal_1_square_2=n_diagonal_1_square_2,
+            n_diagonal_2_square_2=n_diagonal_2_square_2,
+        )
     )
 
     D1 = sc_sphere.incidence_matrix(1).todense()
     D2 = sc_sphere.incidence_matrix(2).todense()
+
     ker_D1 = np.shape(D1)[1] - matrix_rank(D1)
     H1 = ker_D1 - matrix_rank(D2)
 
-    print(D1)
-    print(D2)
+    print(D1.shape)
+    print(D2.shape)
     print("The 1st homology is", H1)
     is_surface = check_surface_homeomorphic(D1, D2)
     print("The complex represents a surface:", is_surface)
+
+    return [tri_square_1, tri_square_2, points_square_1, points_square_2]
 
 
 def save_datapoints(datapoints, folder_path, file_name):
@@ -716,14 +914,42 @@ def main() -> None:
     #     "tori_incidence_matrices.npy",
     # )
 
-    [tri_square_1, tri_square_2] = generate_genus_0_datapoints(
-        n_cycle_1=23,
-        n_cycle_2=20,
-        n_cycle_3=15,
-        n_cycle_4=18,
-        n_interior=30,
-        n_interior_square_2=22,
+    [tri1, tri2, points_square1, points_square2] = generate_genus_0_datapoints(
+        n_cycle_1=5,
+        n_cycle_2=12,
+        n_interior=8,
+        n_cycle_3=7,
+        n_cycle_4=9,
+        n_interior_square_2=8,
+        n_diagonal_1_square_1=5,
+        n_diagonal_2_square_1=7,
+        n_diagonal_1_square_2=4,
+        n_diagonal_2_square_2=3,
     )
+    # points = sample_random_vertices()
+
+    # points_square1 = points[0]
+    # points_square2 = points[1]
+
+    # tri1 = Delaunay(points_square1)
+    # tri2 = Delaunay(points_square2)
+    # Create a figure with two subplots
+    fig, axs = plt.subplots(1, 2)
+
+    # Plot the first triangulation in the first subplot
+    axs[0].triplot(points_square1[:, 0], points_square1[:, 1], tri1.simplices)
+    axs[0].plot(points_square1[:, 0], points_square1[:, 1], "o")
+    axs[0].set_title("Triangulation 1")
+
+    # Plot the second triangulation in the second subplot
+    axs[1].triplot(points_square2[:, 0], points_square2[:, 1], tri2.simplices)
+    axs[1].plot(points_square2[:, 0], points_square2[:, 1], "o")
+    axs[1].set_title("Triangulation 2")
+
+    # Adjust the spacing between subplots
+    plt.tight_layout()
+
+    plt.show()
 
 
 if __name__ == "__main__":
