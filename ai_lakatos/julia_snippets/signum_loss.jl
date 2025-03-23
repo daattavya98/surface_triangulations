93973a8d3d397ba35dab8afd7adf33d519e489f9
daattavya98 <da579@cam.ac.uk>
2025-03-23 16:38:12 +0000

    Logic of implies implementation looks correct

--- ai_lakatos/julia_snippets/signum_loss.jl
+++ ai_lakatos/julia_snippets/signum_loss.jl
@@ -8,12 +8,13 @@ struct SignumLoss <: Function
   complexityWeight::Real
   unusedFunctionPenalty::Real
   punishConstant::Real
+  logicalErrorPenalty::Real
   # tightnessStrength::Real
   # tightnessLengthScale::Real
 end
 
-function SignumLoss(requiredOps=[(1, 2), (2, 3), (2, 4)]; complexityWeight=0.5, unusedFunctionPenalty=1e1, punishConstant=1e3)
-  return SignumLoss(requiredOps, complexityWeight, unusedFunctionPenalty, punishConstant)
+function SignumLoss(requiredOps=[(1, 2), (2, 3), (2, 4)]; complexityWeight=0.5, unusedFunctionPenalty=1e1, punishConstant=1e3, logicalErrorPenalty=1e6)
+  return SignumLoss(requiredOps, complexityWeight, unusedFunctionPenalty, punishConstant, logicalErrorPenalty)
 end
 
 function (loss::SignumLoss)(tree, dataset::Dataset{T,L}, options, idx) where {T,L}
@@ -50,6 +51,28 @@ function (loss::SignumLoss)(tree, dataset::Dataset{T,L}, options, idx) where {T,
     return unusedFunctionCount * loss.unusedFunctionPenalty
   end
 
+  # Require that `implies` always acts on an atomic formula. Both left and right children must either have `logical_neg` or `equals` as an operator.
+  equals_index = 3
+  implies_index = 4
+  logical_neg_index = 6
+
+  num_logical_errors = 0
+  foreach(tree) do node
+    is_implies_node = node.degree == 2 && node.op == implies_index
+    if is_implies_node
+      left_child = node.l
+      right_child = node.r
+      if (left_child.degree == 2 && left_child.op != equals_index) || (left_child.degree == 1 && left_child.op != logical_neg_index) || (left_child.degree == 0) || (right_child.degree == 2 && right_child.op != equals_index) || (right_child.degree == 1 && right_child.op != logical_neg_index) || (right_child.degree == 0)
+        num_logical_errors += 1
+      end
+    end
+  end
+
+  # if num_logical_errors > 0
+  #   # Short circuit return if logical errors are present (that is, `implies` is not used correctly)
+  #   return L(loss.logicalErrorPenalty)
+  # end
+
   # Evaluate expression tree
   y_pred, ok = eval_tree_array(tree, X, options)
   if !ok
@@ -57,6 +80,25 @@ function (loss::SignumLoss)(tree, dataset::Dataset{T,L}, options, idx) where {T,
   end
 
   # Signum loss
-  signumLoss = mean(y_pred .> y) + 1 / complexity * loss.complexityWeight + constant_count * loss.punishConstant
+  signumLoss = mean(y_pred .= y) + 1 / complexity * loss.complexityWeight + constant_count * loss.punishConstant + num_logical_errors * loss.logicalErrorPenalty
   return L(signumLoss)
 end
+
+# # Logical error penalty
+# logical_error_implies = any(tree) do node
+
+#   is_implies_node = node.degree == 2 && node.op == implies_index
+
+#   if is_implies_node
+#     any(node) do node2
+#       (node2.degree == 2 && node2.op != equals_index) || (node2.degree == 1 && node2.op != logical_neg_index) || (node2.degree == 0)
+#     end
+#   else
+#     false
+#   end
+# end
+
+# if logical_error_implies
+#   # Short circuit return if logical errors are present (that is, `implies` is not used correctly)
+#   return L(loss.logicalErrorPenalty)
+# end
--- ai_lakatos/pysr_conjecturing_agent.py
+++ ai_lakatos/pysr_conjecturing_agent.py
@@ -1,8 +1,14 @@
 import os
+from typing import Any
 
 import numpy as np
 import pandas as pd
+import sympy
 from pysr import PySRRegressor, jl
+
+
+class sympy_p(sympy.Function):
+    pass
 
 
 def load_datafile(filename: str) -> np.ndarray:
@@ -22,32 +28,88 @@ def load_datafile(filename: str) -> np.ndarray:
     return dataset
 
 
-def create_regressor_model(loss_func) -> PySRRegressor:
+def create_regressor_model(
+    loss_func: Any, maxsize: int = 15, warmup_maxsize_by: float = 1.5
+) -> PySRRegressor:
 
     model = PySRRegressor(
         populations=32,
         niterations=50,
         ncycles_per_iteration=1000,
-        binary_operators=["+", "*", "logical_or", "logical_and"],
+        binary_operators=["+", "*", "equals", "implies", "-"],
         unary_operators=[
-            "neg",
+            "logical_neg",
         ],
         # elementwise_loss="loss(prediction, target) = (prediction - target)^2",
         # ^ Custom loss function (julia syntax)
         loss_function=loss_func,
+        extra_sympy_mappings={
+            "equals": sympy_p,
+            "implies": sympy_p,
+            "logical_neg": sympy_p,
+        },
+        maxsize=maxsize,
+        warmup_maxsize_by=warmup_maxsize_by,
     )
     return model
 
+
+# Define equality operator
+jl.seval(
+    """
+         function equals(x::T, y::T) where T
+            if x == y
+                return T(1.0)
+            else
+                return T(0.0)
+            end
+         end
+         """
+)
+
+# Define implication operator
+jl.seval(
+    """
+         function implies(x::T, y::T) where T
+            if (x != T(0.0) && x != T(1.0)) || (y != T(0.0) && y != T(1.0))
+                return T(NaN)
+            elseif (x == T(1.0) && y == T(0.0))
+                return T(0.0)
+            else
+                return T(1.0)
+            end
+         end
+         """
+)
+
+# Define logical negation operator
+jl.seval(
+    """
+        function logical_neg(x::T) where T
+            if x == T(0.0)
+                return T(1.0)
+            elseif x == T(1.0)
+                return T(0.0)
+            else
+                return T(NaN)
+            end
+        end
+         """
+)
 
 # Load the signum_loss object from the julia_snippets.signum_loss module
 jl.include("ai_lakatos/julia_snippets/signum_loss.jl")
 # signum_loss = a.signum_loss
 
-loss = jl.seval("""SignumLoss([(1, 1), (2, 1), (2, 2)]; complexityWeight=0.1)""")
+
+loss = jl.seval(
+    """SignumLoss([(1, 1), (2, 1), (2, 3), (2, 4), (2, 5)]; complexityWeight=1.0, unusedFunctionPenalty=1e4,
+                punishConstant=0.0, logicalErrorPenalty=1e3)"""
+)
 
 
-X_sphere = load_datafile("sphere_dataset.csv")[1:, 1:]
-X_torus = load_datafile("torus_dataset.csv")[1:, 1:]
+X_sphere = load_datafile("sphere_dataset.csv")[1:, 1:-1]
+X_torus = load_datafile("torus_dataset.csv")[1:, 1:-1]
 # X = pd.concat([X_sphere, X_torus], axis=0, ignore_index=True)
 X = np.concatenate((X_sphere, X_torus), axis=0)
 
@@ -60,11 +122,10 @@ variable_names = [
     "rank_D2",
     "nullity_D1",
     "nullity_D2",
-    "H1",
 ]
-y = np.zeros(len(X))
+y = np.array([2.0 for _ in range(len(X))])
 
-model = create_regressor_model(loss_func=loss)
+model = create_regressor_model(loss_func=loss, maxsize=30)
 model.fit(X, y, variable_names=variable_names)
 print(model.get_best().equation)
 # print(model.get_best().to_latex())
